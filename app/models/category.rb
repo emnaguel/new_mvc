dad57cac513421ea0d0d30f3035f3ca499a26d8b
Emna Guellaty <eguellaty@gmail.com>
2019-01-11 13:54:04 +0100

    test

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,4 @@
 class Category < ApplicationRecord
   has_many :names
   has_many :items,  through: :names
-
-  validates :name, presence: true, length: { minimum: 5 }
-  validates :description, presence: true
 end
