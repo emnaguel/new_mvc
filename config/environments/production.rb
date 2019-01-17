4b8bdd8aaf7d528a7a56bf25813f0bcaa4f50258
Emna Guellaty <eguellaty@gmail.com>
2019-01-17 18:13:06 +0100

    sendgrid set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,9 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.defaut_url_options = {host: 'https://mvcexos.herokuapp.com/'}
+  config.action_mailer.default_url_options = {host: 'https://mvcexos.herokuapp.com/'}
   config.action_mailer.delivery_method = :smtp
+
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
